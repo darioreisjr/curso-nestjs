1453d6342af8dc2af3632018c84cb33946e1fc21
Dario Reis <guttyerresjunior1995@gmail.com>
2024-10-23 20:56:24 -0300

    feat(products): add products folder and create controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './users/user.controller';
+import { ProductController } from './products/product.controller';
 
 @Module({
   imports: [],
-  controllers: [UserController],
+  controllers: [UserController, ProductController],
   providers: [],
 })
 export class AppModule {}
