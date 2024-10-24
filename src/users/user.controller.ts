02a21993ed5e6334061ef3da32aa663e60ec13da
Dario Reis <guttyerresjunior1995@gmail.com>
2024-10-23 21:30:43 -0300

    feat: add learning and implementation of parameters and queries

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
+type ParamsUser = {
+  id: string;
+  idEmpresa: string;
+};
 @Controller()
 export class UserController {
-  @Get('/helloUser')
-  helloUser() {
-    return 'Seja bem vindo(a) ao curso de NestJs';
+  /**
+   * http://localhost:3000/users/54763545365
+   */
+  @Get('/users/:id/:idEmpresa')
+  findById(@Param() params: ParamsUser) {
+    return 'Usuário do ID' + params.id + 'ID da Empresa ' + params.idEmpresa;
+  }
+
+  /**
+   * hhtp://localhost:3000/users?p=10&r=100
+   */
+  @Get('/users/findByPages')
+  findByPages(@Query() queries: any) {
+    return 'Queries: ' + JSON.stringify(queries);
   }
 }
